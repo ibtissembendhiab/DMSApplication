6867545d1350efbd39778fd4eacdca711fe04174
ibtissembendhiab <ibtissem.bendhiab@esen.tn>
2021-04-12 14:39:10 +0100

    files

--- DMSWebApplication/Controllers/UploadController.cs
+++ DMSWebApplication/Controllers/UploadController.cs
@@ -33,65 +33,65 @@ namespace DMSWebApplication.Controllers
         }
         [HttpPost]
         [Route("upload")]
-        public IActionResult Upload(IFormFile file)
-        {
-            {
+         public IActionResult Upload(IFormFile file)
+         {
+             {
 
-                File f = new File();
-                var filePath = Path.Combine(@"ressources", file.FileName);
+                 File f = new File();
+                 var filePath = Path.Combine(@"ressources", file.FileName);
 
-                if (file.Length > 0)
-                {
-                    var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
-                    //  var fullPath = Path.Combine(pathToSave, fileName);
-                    var dbPath = Path.Combine(fileName);
-                    var serverpath = Path.Combine(fileName);
-                    using (var fileStream = new FileStream(filePath, FileMode.Create))
-                    {
-                        file.CopyTo(fileStream);
-                    }
+                 if (file.Length > 0)
+                 {
+                     var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                     //  var fullPath = Path.Combine(pathToSave, fileName);
+                     var dbPath = Path.Combine(fileName);
+                     var serverpath = Path.Combine(fileName);
+                     using (var fileStream = new FileStream(filePath, FileMode.Create))
+                     {
+                         file.CopyTo(fileStream);
+                     }
 
-                    //f.FileExtension = Path.GetExtension(serverpath);
-                    f.FileName = fileName;
-                    f.FilePath = serverpath;
+                     //f.FileExtension = Path.GetExtension(serverpath);
+                     f.FileName = fileName;
+                     f.FilePath = serverpath;
 
-                    //f.FileDiscription = "file uploaded successfully ";
-                    f.FileSize = file.Length;
-                    f.FileVersion = 1;
+                     //f.FileDiscription = "file uploaded successfully ";
+                     f.FileSize = file.Length;
+                     f.FileVersion = 1;
 
-                  // var userIdClaim = HttpContext.User.Claims.Where(x => x.Type == "UserId").SingleOrDefault().Value;
-                  // var user = _context.Users.Where(u => u.Id == userIdClaim).FirstOrDefault();
+                   // var userIdClaim = HttpContext.User.Claims.Where(x => x.Type == "UserId").SingleOrDefault().Value;
+                   // var user = _context.Users.Where(u => u.Id == userIdClaim).FirstOrDefault();
 
-                    // var folder = _context.Folder.Where(folderid => folderid.FolderId == idfolder).FirstOrDefault();
-                    //folder.ElementNumber = folder.ElementNumber += 1;
-                    //folder.FolderSize = folder.FolderSize + file.Length;
-                    //f.FileFolder = folder;
-                   //  f.FileOwner = user;
-
-
-                    var datenow = DateTime.Now.Date;
-                    var date = datenow.ToString("dd/MM/yyyy");
-
-                    f.UploadDate = DateTime.Now.Date;
-                    f.FileOwner = null;
-                    f.FileFolder = null;
-                    f.FileStatut = "";
-
-                    _context.Add(f);
-
-                    _context.SaveChanges();
+                     // var folder = _context.Folder.Where(folderid => folderid.FolderId == idfolder).FirstOrDefault();
+                     //folder.ElementNumber = folder.ElementNumber += 1;
+                     //folder.FolderSize = folder.FolderSize + file.Length;
+                     //f.FileFolder = folder;
+                    //  f.FileOwner = user;
 
 
-                    return Ok(new { dbPath });
-                }
-                else
-                {
-                    return BadRequest();
-                }
+                     var datenow = DateTime.Now.Date;
+                     var date = datenow.ToString("dd/MM/yyyy");
+
+                     f.UploadDate = DateTime.Now.Date;
+                     f.FileOwner = null;
+                     f.FileFolder = null;
+                     f.FileStatut = "";
+
+                     _context.Add(f);
+
+                     _context.SaveChanges();
 
 
-            }
-        }
+                     return Ok(new { dbPath });
+                 }
+                 else
+                 {
+                     return BadRequest();
+                 }
+
+
+             }
+         }
 
         [HttpGet("download")]
         public async Task<IActionResult> Download([FromQuery] string FileName)
